6fd32bbebe0e2fe52239b0f8b146dcb511500ce6
angel roacho <shadowdevelop@hotmail.com>
2015-12-31 12:01:03 -0600

    fechas

---
+++ RoachoSolutions.Utils/DateTimeUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoachoSolutions.Utils
+{
+    public class DateTimeUtils
+    {
+        public static string TiempoTrascurrido(DateTime Start,DateTime End)
+        {
+            TimeSpan diff = End - Start;
+            if (diff.Days == 0)
+            {
+                return "hoy";
+            }
+            return "ayer";
+        }
+    }
+}
